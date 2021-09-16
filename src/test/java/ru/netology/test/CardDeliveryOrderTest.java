2cc0a6eede88a5ae83f4ea062e88412b32a4be7f
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-09-16 10:34:57 +0300

    Исправлены ошибки

--- src/test/java/ru/netology/test/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/test/CardDeliveryOrderTest.java
@@ -1,17 +1,11 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.SelenideElement;
-
-import com.google.j2objc.annotations.LoopTranslation;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
 
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
